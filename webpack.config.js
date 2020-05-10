ff313ba06d71776d7a6c32b6b4bf07ff77568b6d
Peric Milosevic <supremeruler98@gmail.com>
2020-05-10 23:18:13 +0200

    installed babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist'
-  },
-  devServer: {
-      contentBase: __dirname
-  }
+    entry: ["babel-polyfill", "./src/index.ts"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/dist",
+    },
+    devServer: {
+        contentBase: __dirname,
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
+        ],
+    },
 };
