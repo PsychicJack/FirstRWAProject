ce77a8fc41fb8b0d15960067b6aca3a714eb91f0
Peric Milosevic <supremeruler98@gmail.com>
2020-05-12 00:41:49 +0200

    craeted user class

---
+++ src/classes/user.ts
@@ -0,0 +1,12 @@
+
+
+export class User{
+    penName: string;
+    password: string;
+
+    constructor(penName: string, password: string)
+    {
+        this.penName = penName;
+        this.password = password;
+    }
+}
