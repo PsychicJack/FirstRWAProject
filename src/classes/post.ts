ea4bf276b1d8b65f433849544e084e99e6448fda
Peric Milosevic <supremeruler98@gmail.com>
2020-05-12 00:07:51 +0200

    post class finished

---
+++ src/classes/post.ts
@@ -0,0 +1,29 @@
+import { createDivWithClass } from "../fequentlyUsedFunctions";
+
+export class Post {
+    title: string;
+    pages: string[];
+    author: string;
+    tags: string[];
+
+    constructor(title: string, pages: string[], author: string, tags: string[] = []) {
+        this.title = title;
+        this.pages = pages;
+        this.author = author;
+        this.tags = tags;
+    }
+
+    drawCard(host: HTMLDivElement): HTMLDivElement {
+        const postCard: HTMLDivElement = createDivWithClass(host, "post-card");
+        createDivWithClass(postCard, "post-card-title").innerHTML = this.title;
+        createDivWithClass(postCard, "post-card-page").innerHTML = this.pages[0];
+        return postCard;
+    }
+
+    draw(host: HTMLDivElement): HTMLDivElement{
+        const post : HTMLDivElement =  createDivWithClass(host, "post");
+        createDivWithClass(post, "post-title").innerHTML = this.title;
+        createDivWithClass(post, "post-page").innerHTML = this.pages[0];
+        return post;
+    }
+}
