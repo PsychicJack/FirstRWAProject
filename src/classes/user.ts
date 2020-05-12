9758b259f65006cd37e8b033167131958fe0315a
Peric Milosevic <supremeruler98@gmail.com>
2020-05-12 16:16:21 +0200

    added id to user

--- src/classes/user.ts
+++ src/classes/user.ts
@@ -1,9 +1,11 @@
 export class User {
+    id: number
     penName: string;
     password: string;
 
-    constructor(penName: string, password: string) {
+    constructor(penName: string, password: string, id: number = 0) {
         this.penName = penName;
         this.password = password;
+        this.id = id;
     }
 }
