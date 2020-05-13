346b6cd7b1e1fa720616cbbdee1510c57a96763a
Peric Milosevic <supremeruler98@gmail.com>
2020-05-14 00:02:32 +0200

    added NUMBER_OF_CARDS_PER_LOAD

--- src/services/config.ts
+++ src/services/config.ts
@@ -3,3 +3,4 @@ export const URL_POSTS: string = "http://localhost:3000/posts";
 export const URL_TAGS: string = "http://localhost:3000/tags";
 export const URL_PAGE: string= "http://localhost:8080/?page=";
 export const NUMBER_OF_TAGS_ON_INDEX: number = 6;
+export const NUMBER_OF_CARDS_PER_LOAD: number = 3;
